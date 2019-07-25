33b7a9ae80f3fb7261ecade5b6aaaf56a79832c2
Wyane <wyane.yia@gmail.com>
2019-07-25 14:22:26 +0800

    更新博客

--- deploy_public.sh
+++ deploy_public.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "-----------------开始将public同步到github和coding仓库---------------------"
 git add .
-git commit -m "update blog article"
+git commit -m "更新博客"
 git push -u github master
+echo "同步到github仓库完成..."
 git push -u coding master
+echo "同步到coding仓库完成..."
+echo "--------------------------同步public到仓库结束----------------------------"
 
